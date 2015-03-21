f3b92e7bb90463594c26b0014f5b59e6ea7c5d2e
iluxonchik <iluxon4ik@hotmail.com>
2015-03-21 14:11:51 +0000

    GraphFromInputBuilder refractoring.
    New functions, code is cleaner now.

--- src/Main.java
+++ src/Main.java
@@ -1,130 +1,170 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.StreamTokenizer;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Main.
+ */
 public class Main {
 
-	public static void main(String[] args)  throws IOException {
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
 			
-			//System.setIn(new FileInputStream("in.txt"));
-			Graph g = GraphFromInputBuilder.build();
+			GraphFromInputBuilder builder = new GraphFromInputBuilder(System.in);
+			Graph g = null;
 			
-			//System.out.println("V: " + g.getV() + " E: " + g.getE());
-			ErdosNumber en = new ErdosNumber(g, GraphFromInputBuilder.source);
+			try {
+				g = builder.build();
+			} catch (IOException e) {
+				System.out.println("Error while building the graph from the input file.");
+				e.printStackTrace();
+			}
+			
+			ErdosNumber en = new ErdosNumber(g, builder.getSource() - 1);
 			en.bfs();
 			
-			en.printOutput();
-			
-			//char[] buff = new char[20];
-			//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			//GraphFromInputBuilder.readLine(buff, br);
+			try {
+				en.printOutput();
+			} catch (IOException e) {
+				System.out.println("Error while printing the output.");
+				e.printStackTrace();
+			}
 	}
 }
 
 class Graph {
 	
-	private int V;
-	private int E;
-	private LinkedList<Integer> node[];
+	private int v; // number of vertices
+	private int e; // number of edges
+	private ArrayList<LinkedList<Integer>> node;
 	
 	/**
 	 * Instantiates a new graph.
 	 *
 	 * @param v number of vertices 
 	 */
-	@SuppressWarnings("unchecked")
-	public Graph (int v) {
-		// TODO: argument checks
-		this.V = v;
-		this.E = 0;
+	public Graph (int v, int e) {
+		this.v = v;
+		this.e = e;
 		
 		// Allocate memory for nodes
-		node = new LinkedList[v];
+		node = new ArrayList<LinkedList<Integer>>(v);
 
-		for (int i = 0; i < V; i++) {
+		for (int i = 0; i < v; i++) {
 			// Create an adjacency list for every node
-			node[i] = new LinkedList<Integer>();
+			node.add(new LinkedList<Integer>());
 		}
 	}
 	
 	public void addEdge(int u, int v) {
-		// TODO: argument checks
-		node[u].add(v);
-		node[v].add(u);
+		node.get(u).add(v);
+		node.get(v).add(u);
 	}
 	
 	/**
-	 * Returns an iterator over the adjacency list of a vertex. 
+	 * Returns the adjacency list of a vertex. 
 	 *
 	 * @param v vertex to access
-	 * @return iterator over the adjacency list of vertex v
+	 * @return adjacency list of vertex v
 	 */
 	public LinkedList<Integer> getAdj(int v) {
-		return node[v];
+		return node.get(v);
 	}
 	
+	// Getters
+	public int getE() { return this.e; }
+	public int getV() { return this.v; }
+
+	// Setters
+	public void setE(int e) { this.e = e; }
+	public void setV(int v) { this.v = v; }
 	
-	public void setE(int e) {
-		this.E = e;
-	}
-	
-	public int getE() { return this.E; }
-	public int getV() { return this.V; }
 }
 
 class GraphFromInputBuilder{
 	
-	public static int source = 0;
+	private InputStream is;
+	private int source; // source for BFS
+	private int v; // number of vertices
+	private int e; // number of edges
 	
-	public static Graph build() {
-		
-		int v;
-		int e;
-		int s;
+	
+	public GraphFromInputBuilder(InputStream is) { this.is = is; }
+	
+	/**
+	 * Builds a graph using the specified InputStream as an input. 
+	 *
+	 * @param is the InputStream from where the input will be read
+	 * @return an instance of a Graph
+	 * @throws IOException if an I/O error in parseNumberFromStream occurs
+	 */
+	public Graph build() throws IOException{
 		Graph g = null;
 		
 		StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-        st.parseNumbers();
-        try{
-                st.nextToken();
-                v = (int) st.nval;
-        		g = new Graph(v);
-                st.nextToken();
-                e = (int) st.nval;
-                g.setE(e);
-                st.nextToken();
-                s = (int) st.nval;
-                source = s-1;
-                int i;
-                i = 0;
-                
-                int u;
-                int w;
-                
-                while(i < e)
-                {
-                        st.nextToken();
-                        u = (int) st.nval;
-                        st.nextToken();
-                        w = (int) st.nval;
-                        g.addEdge(u-1, w-1);
-                        i++;
-                }
-        } catch (IOException excep){
-                System.err.println("Something went wrong.");
-        }
-		
+        st.parseNumbers();        
+        
+        v = parseNumberFromStream(st);
+        e = parseNumberFromStream(st);
+        source = parseNumberFromStream(st);
+        g = new Graph(v, e);
+        parseGraphEdges(st, g);
+        
 		return g;
 	}
 	
-	public static int readLine(char[] buff, BufferedReader br) throws IOException{
+	/**
+	 * Parses the number from the InputStream using the StreamTokenizer passed as an argument. 
+	 *
+	 * @param st the StreamTokenizer to use
+	 * @return the parsed integer
+	 * @throws IOException if an I/O error in StreamTokenizer.nextToken() occurs
+	 */
+	private int parseNumberFromStream(StreamTokenizer st) throws IOException{
+			st.nextToken();
+			return (int) st.nval;
+	}
+	
+	/**
+	 * Parses the edges from the InputStream using the st and adds them to the graph g. 
+	 *
+	 * @param st the StreamTokenizer to use
+	 * @param g the Graph to add the edges to
+	 * @throws IOException if an I/O error in parseNumberFromStream() occurs
+	 */
+	private void parseGraphEdges(StreamTokenizer st, Graph g) throws IOException{
+		final int e = g.getE();
+        for (int i = 0, w = 0, u = 0; i < e; i++) {
+            u = parseNumberFromStream(st);
+            w = parseNumberFromStream(st);
+            g.addEdge(u-1, w-1);
+         }
+	}
+	
+
+	// Getters
+	public InputStream getInputStream() { return is; }
+	public int getSource() { return source; }
+	
+	// Setters
+	public void setInputStream(InputStream is) { this.is = is; }
+	
+	// Not used
+	public int readLine(char[] buff, BufferedReader br) throws IOException{
 		int index = 0;
 		int charRead;
 		int firstChar;
